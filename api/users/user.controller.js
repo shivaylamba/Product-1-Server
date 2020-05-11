f5bbeefa47c9b6364c0ba31e59e66187f5057016
aakashrajput <aakashsingh1999@gmail.com>
2020-05-12 02:15:02 +0530

    Integrated JsonWebToken

--- api/users/user.controller.js
+++ api/users/user.controller.js
@@ -1,5 +1,6 @@
-const {create} = require("./user.service");
-const {genSaltSync, hashSync} = require("bcrypt");
+const {create, getUsers, getUsersById, updateUser, deleteUser, getUserByEmail} = require("./user.service");
+const {genSaltSync, hashSync, compareSync} = require("bcrypt");
+const {sign} = require("jsonwebtoken");
 
 module.exports  = {
     createUser: (req,res) => {
@@ -19,5 +20,113 @@ module.exports  = {
                 data: results
             });
         }); 
+    },
+
+    getUsersById: (req,res) => {
+        const id = req.params.id;
+        getUsersById(id, (err,results) => {
+            if(err) {
+                console.log(err);
+                return;
+            }
+            if(!results) {
+                return res.json({
+                    success:0,
+                    message:"Record does not exist!"
+                });
+            }
+            return res.json({
+                success:1,
+                data:results
+            });
+        });
+    },
+
+    getUsers: (req,res) => {
+        getUsers((err,results) => {
+            if(err) {
+                console.log(err);
+                return;
+            }
+            return res.json({
+                success:1,
+                data:results
+            });
+        });
+    },
+
+    updateUser: (req,res) => {
+        const body = req.body;
+        const salt = genSaltSync(10);
+        body.password = hashSync(body.password, salt);
+        updateUser(body, (err, results) => {
+            if(err) {
+                console.log(err);
+                return;
+            }
+            if(!results) {
+                return res.json({
+                    success:0,
+                    message:"Failed to Update Users, User Does not Exist."
+                });
+            }
+            return res.json({
+                success:1,
+                message: "Updated Successfully"
+            });
+        });
+    },
+
+    deleteUser: (req,res) => {
+        const data = req.body;
+        deleteUser(data, (err,results) => {
+            if(err) {
+                console.log(err);
+                return;
+            }
+            if(!results) {
+                return res.json({
+                    success:0,
+                    message:"Record does not exist!"
+                });
+            }
+            return res.json({
+                success:1,
+                message: "User Deleted Successfully"
+            });
+        });
+    },
+
+    login: (req,res) => {
+        const body = req.body;
+        getUserByEmail(body.email, (err,results) => {
+            if(err) {
+                console.log(err);
+                return;
+            }
+            if(!results) {
+                return res.json({
+                    success:0,
+                    message:"Invalid Email or Password"
+                });
+            }
+            const result = compareSync(body.password, results.password);
+            if (result) {
+                results.password = undefined;
+                const jsontoken = sign({ result: results}, "XtremeWorkers2020", {
+                    expiresIn: "1h"
+                });
+                return res.json({
+                    success:1,
+                    message: "User LogeIn Successfully",
+                    token: jsontoken
+                });
+            } else {
+                return res.json({
+                    success:0,
+                    message:"Invalid Email or Password"
+                });
+            }
+        });
     }
 }
--- api/users/user.router.js
+++ api/users/user.router.js
@@ -1,7 +1,13 @@
-const { createUser } = require("./user.controller");
+const { createUser, getUsers, getUsersById, updateUser, deleteUser, login } = require("./user.controller");
 const router = require("express").Router();
+const {checkToken} = require("../../auth/token_validation");
 
 
-router.post("/", createUser);
+router.post("/", checkToken, createUser);
+router.get("/", checkToken, getUsers);
+router.get("/:id", checkToken, getUsersById);
+router.patch("/", checkToken, updateUser);
+router.delete("/", checkToken, deleteUser);
+router.post("/login", login);
 
 module.exports = router;
